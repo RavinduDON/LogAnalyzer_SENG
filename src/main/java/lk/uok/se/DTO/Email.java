31ce940f3b8072e4a0b322d0d4a0a5d02365c978
ravinduDon <ravindugunarathna@gmail.com>
2020-09-14 13:23:03 +0530

    email DTO created

--- src/main/java/lk/uok/se/DTO/Email.java
+++ src/main/java/lk/uok/se/DTO/Email.java
@@ -1,4 +1,30 @@
 package lk.uok.se.DTO;
 
 public class Email {
+    private String address;
+    private String errorMsg;
+
+    public Email() {
+    }
+
+    public Email(String address, String errorMsg) {
+        this.address = address;
+        this.errorMsg = errorMsg;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getErrorMsg() {
+        return errorMsg;
+    }
+
+    public void setErrorMsg(String errorMsg) {
+        this.errorMsg = errorMsg;
+    }
 }
