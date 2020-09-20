5cb4a5a62e8694055e66f99a40113391b81c0558
chathurikadhananjani <57212615+chathurikadhananjani@users.noreply.github.com>
2020-09-20 19:10:07 +0530

    test commit

--- src/test/java/lk/uok/se/Mail/MailSenderTest.java
+++ src/test/java/lk/uok/se/Mail/MailSenderTest.java
@@ -10,6 +10,15 @@ public class MailSenderTest {
         String address="";
         MailSender sender=new MailSender();
 
+    }
+    @Test
+    public void When_Email_Address_is_invalid(){
+        ArrayList<String> sender=new ArrayList<String>();
+
+        Authorization authorization=new MailSenderTest();
+        ArrayList<String> sender2=new ArrayList<String>;
+
+        assertArrayEquals(sender2.toArray(),sender.toArray());
 
     }
 }
